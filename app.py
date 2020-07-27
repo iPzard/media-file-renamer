07203a59fffb244b4675e76a14186e9e51319268
unknown <wade.daniel.w@gmail.com>
2020-07-26 22:02:53 -0700

    updating JSDoc documentation

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 @app.route('/<command>', methods=['GET', 'POST'])
 def index(command):
 
+  """ Request to get files:
+  Request to get the directory path and list
+  of files to rename from user's computer.
+  """
+
   if command == 'get_files':
     files = None
 
@@ -16,9 +21,18 @@ def index(command):
     # Return list/array of file names
     return jsonify(files)
 
+
+  """ Request to rename files:
+  Request to rename files in the directory
+  path provided with new names passed in
+  the request data.
+  """
+
   if command == 'rename_files':
     print(request.data)
 
+    return 'Hello!'
+
 # Available for manual testing
-if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8081)
+# if __name__ == '__main__':
+#   app.run(host='127.0.0.1', port=8081)
